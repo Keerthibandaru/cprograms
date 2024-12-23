0f6613a40f73c1c333b9d6035cf9c8a66786c24d
Keerthibandaru <kkeerthibandaru@gmail.com>
2024-12-23 12:29:47 +0530

    Create numfrom0to10and10to0.c

---
+++ numfrom0to10and10to0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int i,n=0;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    for(i=1;i<=10;++i)
+{
+    printf("%d",i);
+}
+for(i=10;i>0;--i)
+{
+    printf("%d",i);
+}
+}
