f0d0fc3f910f00c309cd4b34a66b32d0eb92346b
Keerthibandaru <kkeerthibandaru@gmail.com>
2024-12-30 08:02:30 +0530

    Create Sumofdigits.c

---
+++ Sumofdigits.c
@@ -0,0 +1,29 @@
+include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n >=1&& n<=9) {
+        if(n==1)
+             printf("one\n"); 
+             if(n==2)
+           printf("two\n"); 
+           if(n==3)
+            printf("three\n");
+            if(n==4)
+            printf("four\n");
+            if(n==5)
+            printf("five\n"); 
+            if(n==6)
+            ("six\n"); 
+            if(n==7)
+            printf("seven\n"); 
+            if(n==8)
+            printf("eight\n"); 
+            if(n==9)
+            printf("nine\n");
+        }
+    else {
+        printf("Greater than 9\n");
+    }
+}
